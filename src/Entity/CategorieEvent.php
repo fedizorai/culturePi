66e8d9d450ccf23053820b2c45a183639cd4b834
fedi <fedi.zorai@esprit.tn>
2024-02-18 18:32:16 +0100

    configured database .env + make entity categoryEvent & Evenement

---
+++ src/Entity/CategorieEvent.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieEventRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieEventRepository::class)]
+class CategorieEvent
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $descriptionCategorieEvent = null;
+
+    #[ORM\OneToMany(mappedBy: 'categorie', targetEntity: Evenement::class)]
+    private Collection $evenements;
+
+    public function __construct()
+    {
+        $this->evenements = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescriptionCategorieEvent(): ?string
+    {
+        return $this->descriptionCategorieEvent;
+    }
+
+    public function setDescriptionCategorieEvent(?string $descriptionCategorieEvent): static
+    {
+        $this->descriptionCategorieEvent = $descriptionCategorieEvent;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Evenement>
+     */
+    public function getEvenements(): Collection
+    {
+        return $this->evenements;
+    }
+
+    public function addEvenement(Evenement $evenement): static
+    {
+        if (!$this->evenements->contains($evenement)) {
+            $this->evenements->add($evenement);
+            $evenement->setCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEvenement(Evenement $evenement): static
+    {
+        if ($this->evenements->removeElement($evenement)) {
+            // set the owning side to null (unless already changed)
+            if ($evenement->getCategorie() === $this) {
+                $evenement->setCategorie(null);
+            }
+        }
+
+        return $this;
+    }
+}
