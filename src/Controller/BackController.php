2b1d9e20c56c14ca60acb64038107292f6a42205
Asmmouch <115111316+Asmmouch@users.noreply.github.com>
2024-03-03 00:49:52 +0100

    back
    amelioration

--- src/Controller/BackController.php
+++ src/Controller/BackController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 use App\Repository\PUBLICATIONRepository;
+use App\Repository\COMMENTAIRERepository;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,10 +25,13 @@ class BackController extends AbstractController
         ]);
     }
     #[Route('/BACKK', name: 'app_dashboardd')]
-    public function testt(PUBLICATIONRepository $pUBLICATIONRepository): Response
-    {
+    public function testt(PUBLICATIONRepository $pUBLICATIONRepository,COMMENTAIRERepository $cOMMENTAIRERepository): Response
+    {    $commentaires = $cOMMENTAIRERepository->findAll();
+
         return $this->render('publication/index2.html.twig', [
             'p_u_b_l_i_c_a_t_i_o_ns' => $pUBLICATIONRepository->findAll(),
+            'c_o_m_m_e_n_t_a_i_r_es' => $commentaires,
+
 
         ]);
     }
